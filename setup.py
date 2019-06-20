f5b400925cd263d6d795ba5a7086ffb5b2389e95
Maximilian Ernestus <maximilian@ernestus.de>
2019-06-20 15:51:27 +0200

    Adding more metadata to the setup.py and shrinking the requirements.txt

--- setup.py
+++ setup.py
@@ -2,11 +2,25 @@ from setuptools import setup
 
 setup(
     name='QuadrotorEnvironment',
-    version='1.0.0',
-    packages=['quadrotor_environment', 'quadrotor_environment.tests'],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    packages=['quadrotor_environment'],
     url='https://github.com/erniejunior/QuadrotorEnvironment',
     license='GPL',
     author='Maximilian Ernestus',
     author_email='maximilian@ernestus.de',
-    description='OpenAI Gym Environment for Quadrotors'
+    description='OpenAI Gym Environment for Quadrotors',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='quadrotor quadcopter reinforcement learning gym',
+    python_requires='>=3',
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "numpy-quaternion",
+        "sortedcontainers",
+        "gym",
+    ]
 )
