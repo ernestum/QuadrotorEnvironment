96d9438b91847d18749ee21974e5f6945963b2b8
Maximilian Ernestus <maximilian@ernestus.de>
2019-06-20 14:52:40 +0200

    Adding a setup.py and automatic pypi deployment.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='QuadrotorEnvironment',
+    version='1.0.0',
+    packages=['quadrotor_environment', 'quadrotor_environment.tests'],
+    url='https://github.com/erniejunior/QuadrotorEnvironment',
+    license='GPL',
+    author='Maximilian Ernestus',
+    author_email='maximilian@ernestus.de',
+    description='OpenAI Gym Environment for Quadrotors'
+)
